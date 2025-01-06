31c172e478816708a2ce263288aa90fe2250847a
Roshandeep Barik <76261874+roshandeep35@users.noreply.github.com>
2025-01-06 15:38:21 +0530

    (Enhance): pop up alert box changed

--- frontend/src/components/confirmationPopup.js
+++ frontend/src/components/confirmationPopup.js
@@ -1,16 +1,3 @@
-// export const ConfirmationPopup = ({ show, details, onClose, onConfirm }) => {
-//     if (!show) return null;
-
-//     return (
-//         <div className="confirmation-popup">
-//             <h3>Confirm Transfer</h3>
-//             <pre>{JSON.stringify(details, null, 2)}</pre>
-//             <button onClick={onConfirm}>Confirm</button>
-//             <button onClick={onClose}>Cancel</button>
-//         </div>
-//     );
-// };
-
 import React from 'react';
 import '../componentStyles/confirmationPopup.css'
 
