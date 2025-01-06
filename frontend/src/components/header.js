cb7aef2947f0e4ad7b44bf74f2ebaa8efb691040
Roshandeep Barik <76261874+roshandeep35@users.noreply.github.com>
2025-01-06 14:36:43 +0530

    (Add): Footer and changes in Header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import '../componentStyles/header.css';
 
 const Header = () => {
     return (
         <header className="header">
-            <div className="logo">MyLogo</div>
-            <nav>
-                <button>History</button>
-                <button>Connect Wallet</button>
+            <div className="logo">CrossChainX</div>
+            <nav className="nav-links">
+                <button className="nav-btn">History</button>
+                <button className="nav-btn">Connect Wallet</button>
                 <button className="hamburger">☰</button>
             </nav>
         </header>
     );
 };
-export default  Header;
+
+export default Header;
