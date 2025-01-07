a4c82e6a0307d2c65bda484d6ecb98664b792587
Roshandeep Barik <76261874+roshandeep35@users.noreply.github.com>
2025-01-07 10:17:35 +0530

    (Enhance): Transfer Type Selector

--- frontend/src/components/transferTypeSelector.js
+++ frontend/src/components/transferTypeSelector.js
@@ -1,10 +1,38 @@
 import '../componentStyles/transferTypeSelector.css'
 export const TransferTypeSelector = ({ selectedType, setSelectedType }) => {
-    return (
-        <div className="transfer-type-selector">
-            <button onClick={() => setSelectedType('message')} className={selectedType === 'message' ? 'active' : ''}>Message</button>
-            <button onClick={() => setSelectedType('token')} className={selectedType === 'token' ? 'active' : ''}>Token</button>
-            <button onClick={() => setSelectedType('nft')} className={selectedType === 'nft' ? 'active' : ''}>NFT</button>
+  return (
+    
+      <div className="transfer-type-selector">
+        <div>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              checked={selectedType === 'message'}
+              onChange={() => setSelectedType('message')}
+            />
+            <span>Message</span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              checked={selectedType === 'nft'}
+              onChange={() => setSelectedType('nft')}
+            />
+            <span>NFT</span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              checked={selectedType === 'token'}
+              onChange={() => setSelectedType('token')}
+            />
+            <span>Token</span>
+          </label>
         </div>
-    );
+      </div>
+    
+  );
 };
